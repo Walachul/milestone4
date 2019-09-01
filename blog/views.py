3334dca613da38f0ee3270bf53dda1310420e3b3
Samuel <42890101+Walachul@users.noreply.github.com>
2019-09-01 18:55:24 +0200

    Registered home and blog apps as django apps. Created testing routes for home and blog apps.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    """BLOG HOme page"""
+
+    return HttpResponse("<h1>Blog Home</h1>")
+
+
+def about(request):
+    """BLOG HOme page"""
+
+    return HttpResponse("<h1>Blog About</h1>")
+
 
 # Create your views here.
