f0f907f026af73130ba85c67afcccff3304524b6
Samuel <42890101+Walachul@users.noreply.github.com>
2019-09-28 22:25:10 +0200

    Redefined home view to better represent functionality and what is displayed on the home template. Created view to display a single post into a separate template.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r"^$", views.home, name="blog-home")]
+urlpatterns = [url(r"^$", views.render_posts, name="blog-home")]
 
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
 
-def home(request):
-    """BLOG Home page"""
-    posts = Post.objects.all()
-    return render(request, "home.html", {"title": "Blog"}, {"posts": posts})
+def render_posts(request):
+    """Render all posts in the home page template of the Blog app"""
+
+    posts = Post.objects.filter(dateAdded).order_by("-dateAdded")
+    return render(request, "home.html", {"title": "Blog Home"}, {"posts": posts})
 
 
-# Create your views here.
+def details_one_post(request, pk):
+
+    """Render a single post with more details
+     about it based  on ID or return 404 if not found"""
+
+    post = get_object_or_404(Post, pk=pk)
+    post.save()
+    return render(request, "post_details", {"title": post.title}, {"post": post})
+
+
+def create_edit_post(request, pk=none):
+
+    """Create a post or edit one based on ID"""
+
+    return render(request, "home.html", {"title": "Blog Home"}, {"posts": posts})
