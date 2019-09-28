4019014bcf201956e56989d21c791d994ec6dc90
Samuel <42890101+Walachul@users.noreply.github.com>
2019-09-28 22:08:58 +0200

    Created model Post for the Blog app. Modified home template to render the posts there.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    dateAdded = models.DateTimeField(default=timezone.now)
+    # If User(author) of post gets deleted, his post(s) is deleted as well.
+    authorPost = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def home(request):
     """BLOG Home page"""
-
-    return render(request, "home.html", {"title": "Blog"})
+    posts = Post.objects.all()
+    return render(request, "home.html", {"title": "Blog"}, {"posts": posts})
 
 
 # Create your views here.
