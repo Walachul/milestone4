e342cbbbc7cd0c2a084ca8a6cf11e747be272aef
Samuel <42890101+Walachul@users.noreply.github.com>
2019-09-28 22:46:26 +0200

    Created view for rendering a single post, and create or update a post. Modified url file for blog app to connect the views.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r"^$", views.render_posts, name="blog-home")]
+urlpatterns = [
+    url(r"^$", views.render_posts, name="blog-home"),
+    url(r"^(?P<pk>\d+)/$", views.details_one_post, name="blog-details-post"),
+    url(r"^new/$", views.create_edit_post, name="blog-new-post"),
+    url(r"^(?<pk>\d+)/$", views.create_edit_post, name="blog-edit-post"),
+]
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import BlogForm
 
 
 def render_posts(request):
@@ -21,6 +22,14 @@ def details_one_post(request, pk):
 
 def create_edit_post(request, pk=none):
 
-    """Create a post or edit one based on ID"""
+    """Create a post or edit one based on ID status if null or not."""
 
-    return render(request, "home.html", {"title": "Blog Home"}, {"posts": posts})
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = BlogForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(details_one_post, post.pk)
+        else:
+            form = BlogForm(instance=post)
+        return render(request, "blog_form.html", {"title": "Blog Form"}, {"form": form})
