d66848bea8fc4fe27167f99864e55358ef44fb80
Samuel <42890101+Walachul@users.noreply.github.com>
2019-09-29 14:59:10 +0200

    Created login view and logout template.

--- users/views.py
+++ users/views.py
@@ -11,10 +11,31 @@ def register(request):
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(
-                request, f"The account for {username} was created successfully!"
+                request,
+                f"The account for {username} was created successfully! You are now able to login",
             )
-            return redirect("home-home")
+            return redirect("login")
     else:
         form = RegisterUserForm()
     return render(request, "register.html", {"form": form})
 
+def login(request):
+    """ Return login page """
+    if request.user.is_authenticated:
+        return redirect(reverse("home-home"))
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(
+                username=request.POST["username"], password=request.POST["password"]
+            )
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+                return redirect(reverse("home"))
+            else:
+                form.add_error(None, "Your username or password is incorrect!")
+    else:
+        form = UserLoginForm()
+
+    return render(request, "login.html", {"form": form})
