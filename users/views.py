48cb7c32474a4fc5a59d148ad9676929aadc5260
Samuel <42890101+Walachul@users.noreply.github.com>
2019-09-29 13:01:21 +0200

    Improved register view by validating the register form if we get the POST request and notifying the user that account was created.

--- users/views.py
+++ users/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
-    """Register users"""
-
-    form = UserCreationForm()
+    """Register users view"""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"The account for {username} was created successfully!"
+            )
+            return redirect("home-home")
+    else:
+        form = UserCreationForm()
     return render(request, "register.html", {"form": form})
 
