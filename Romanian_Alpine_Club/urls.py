ccd69ffd354b4272179f438f9efc48572819e8e4
Samuel <42890101+Walachul@users.noreply.github.com>
2019-10-02 19:34:26 +0200

    Modified main urls.py by importing the django built in view Login and Logout so that the path to access it is localhost:8000/login and not localhost:8000/users/login. Implemented this because I tested as unauthenticated to access localhost:8000/logout and localhost:8000/profile but django could not find the url there. Test now is successful.

--- Romanian_Alpine_Club/urls.py
+++ Romanian_Alpine_Club/urls.py
@@ -18,12 +18,23 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from home import urls as urls_home
 from blog import urls as urls_blog
-from users import urls as urls_users
+from users import views as user_views
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
     url(r"^", include(urls_home)),
+    url("register/", user_views.register, name="register"),
+    url("profile/", user_views.profile, name="profile"),
+    url(
+        "login/",
+        auth_views.LoginView.as_view(template_name="users/login.html"),
+        name="login",
+    ),
+    url(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="users/logout.html"),
+        name="logout",
+    ),
     url(r"^blog/", include(urls_blog)),
-    url(r"^users/", include(urls_users)),
 ]
 
