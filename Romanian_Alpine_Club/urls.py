a3a9dcfeb19b73136312c6326edf8b336fd5f6a1
Samuel <42890101+Walachul@users.noreply.github.com>
2019-10-03 21:47:24 +0200

    Registered to Romanian_Alpine_club/urls.py the MEDIA_ROOT and MEDIA_URL.

--- Romanian_Alpine_Club/urls.py
+++ Romanian_Alpine_Club/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from home import urls as urls_home
@@ -38,3 +40,7 @@ urlpatterns = [
     url(r"^blog/", include(urls_blog)),
 ]
 
+"""To run in development and DEBUG mode is True"""
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
