4eec36c738412439bee4fcfa2c1f6d6c5c90f100
Samuel <42890101+Walachul@users.noreply.github.com>
2019-10-03 22:46:08 +0200

    Creates forms for updating user information from his profile page.

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class RegisterUserForm(UserCreationForm):
@@ -28,3 +29,29 @@ class UserLoginForm(forms.Form):
 
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField()
+    homeAddress = forms.CharField(max_length=200)
+    firstName = forms.CharField(max_length=50)
+    lastName = forms.CharField(max_length=50)
+    phoneNumber = forms.CharField(max_length=20)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "firstName",
+            "lastName",
+            "homeAddress",
+            "phoneNumber",
+        ]
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["profileImage"]
+
