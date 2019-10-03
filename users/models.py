3119b824a0ec2f61b0a31fe0d756e428dbe5d771
Samuel <42890101+Walachul@users.noreply.github.com>
2019-10-03 19:41:26 +0200

    Extended user profile with profileImage field, where a user can change/upload his own profile pic.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Extending the user profile by adding image field.
+        On delete argument CASCADE > when the user is deleted, his profile is deleted also,
+        but if profile is deleted, user is not."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profileImage = models.ImageField(default="default.jpg", upload_to="profile_images")
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
