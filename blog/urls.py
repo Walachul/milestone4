a7a17f198e713aef116697e889678f833ac3835e
Samuel <42890101+Walachul@users.noreply.github.com>
2019-10-04 09:33:20 +0200

    Edited route blog-details-post to access the view PostDetailView.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from .views import PostListView
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
     url(r"^$", PostListView.as_view(), name="blog-home"),
-    url(r"^(?P<pk>\d+)/$", views.details_one_post, name="blog-details-post"),
+    url(r"^post/<int:pk>/", PostDetailView.as_view(), name="blog-details-post"),
     url(r"^new/$", views.create_edit_post, name="blog-new-post"),
     url(r"^(?P<pk>\d+)/edit/$", views.create_edit_post, name="blog-edit-post"),
 ]
