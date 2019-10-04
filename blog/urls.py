6bcf25346fffb370882bdc215ed5ecf65904e1b9
Samuel <42890101+Walachul@users.noreply.github.com>
2019-10-04 09:54:20 +0200

    Registered the new PostCreateView to url patterns.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
 
 urlpatterns = [
     url(r"^$", PostListView.as_view(), name="blog-home"),
     url(r"^(?P<pk>\d+)/$", PostDetailView.as_view(), name="blog-details-post"),
-    url(r"^new/$", views.create_edit_post, name="blog-new-post"),
+    url(r"^new/$", PostCreateView.as_view(), name="blog-new-post"),
     url(r"^(?P<pk>\d+)/edit/$", views.create_edit_post, name="blog-edit-post"),
 ]
 
