ac99cd0437b807f226aa76849622f7a239974a42
Samuel <42890101+Walachul@users.noreply.github.com>
2019-10-05 11:51:36 +0200

    Created template for password_reset_confirm. Setup of variables in settings.py to connect and send emails to gmail.

--- Romanian_Alpine_Club/settings.py
+++ Romanian_Alpine_Club/settings.py
@@ -124,3 +124,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 LOGIN_REDIRECT_URL = "home-home"
 LOGIN_URL = "login"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
+
