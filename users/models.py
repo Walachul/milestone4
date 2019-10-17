1438c79bc3ce3bec2c4c7fd41aab724bfc4b6575
Samuel <42890101+Walachul@users.noreply.github.com>
2019-10-17 14:40:51 +0200

    Extending the user registration model by extending it with Profile model. Added additional fields required for registration that will be saved into the db.

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from PIL import Image
 
 
@@ -9,10 +11,26 @@ class Profile(models.Model):
         but if profile is deleted, user is not."""
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Additional information required for registration
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    birthDate = models.DateField(null=True, blank=True)
+    homeAddress = models.CharField(max_length=200)
+    phoneNumber = models.CharField(max_length=20)
     profileImage = models.ImageField(default="default.jpg", upload_to="profile_images")
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    """
+    Profile is created using a Signal.
+    """
+
+    @receiver(post_save, sender=User)
+    def update_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
 
     """Override save function in order to resize the image,
         so that the file system will not get loaded with big files,
