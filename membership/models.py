13a2f4fa7c94048c8bf5a2ee37e14b99190a6cf6
Samuel <42890101+Walachul@users.noreply.github.com>
2019-11-17 15:37:54 +0100

    Register membership app in the settings of Django. Created Membership model.

--- membership/models.py
+++ membership/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+Membership_Choices = ["Regular membership", "Student membership", "Retired membership"]
+
+"""Membership based on 3 choices. 
+   The users will have the same access to the app,
+   only the price will be different based on their status."""
+
+
+class Membership(models.Model):
+    membership_type = models.CharField(
+        choices=Membership_Choices, default="Regular membership", max_length=30
+    )
+    price = models.IntegerField(default=100)
+    stripe_plan_id = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.membership_type
+
