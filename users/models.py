dc4b9b1129e4aa42d7c2ad9569f8b82f1a28b090
Samuel <42890101+Walachul@users.noreply.github.com>
2019-11-17 14:56:36 +0100

    Removed from models the user cardID code.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+import io
+from django.core.files.storage import default_storage as storage
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image, ImageDraw
@@ -16,10 +18,9 @@ class Profile(models.Model):
     )
     phoneNumber = models.CharField(blank=True, max_length=20, default="0766 xxx xxx")
     profileImage = models.ImageField(default="default.jpg", upload_to="profile_images")
-
-    """Implementing generation of card ID per user """
     cardID = models.ImageField(
-        default="default_card.png", upload_to="profile_images/card_ID"
+        default="profile_images/card_ID/default_card.png",
+        upload_to="profile_images/card_ID",
     )
 
     def __str__(self):
@@ -27,33 +28,91 @@ class Profile(models.Model):
 
     """Override save function in order to resize the image,
         so that the file system will not get loaded with big files,
-        affecting performance."""
+        affecting performance. The resized image is now saved also on S3 Amazon"""
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        imgProfile = Image.open(self.profileImage.path)
+        imgProfile_read = storage.open(self.profileImage.name, "r")
+        imgProfile = Image.open(imgProfile_read)
         if imgProfile.height > 300 or imgProfile.width > 300:
-            outputSize = (300, 300)
+            outputSize = (180, 135)
             imgProfile.thumbnail(outputSize)
-            imgProfile.save(self.profileImage.path)
-        imgProfile_w, imgProfile_h = imgProfile.size
-        imgCard = Image.new("RGB", (600, 400), color="#00AEEF")
-        imgCard_w, imgCard_h = imgCard.size
-        offset = ((imgCard_w - imgProfile_w) // 2, (imgCard_h - imgProfile_h) // 2)
+            in_mem_file = io.BytesIO()
+            imgProfile.save(in_mem_file, format="JPEG")
+            img_write = storage.open(self.profileImage.name, "w+")
+            img_write.write(in_mem_file.getvalue())
+            img_write.close()
 
-        """
-        Write user info on the new created image
-        """
-        d = ImageDraw.Draw(imgCard)
+        # imgProfile_w, imgProfile_h = imgProfile.size
+        # logoImg = Image.open(
+        #     "static/img/logo/Romanian_Alpine_Club_Logo_transparent.png", "r"
+        # )
+        # logoImg_w, logoImg_h = logoImg.size
+        # imgCard = Image.new("RGB", (600, 350), color="#00AEEF")
+        # imgCard_w, imgCard_h = imgCard.size
+        # offsetlogo = (40, 10)
+        # offset = (40, 190)
 
-        d.text((40, 30), User.first_name, fill=(255, 255, 255))
-        d.text((70, 30), User.last_name, fill=(255, 255, 255))
-        d.text((40, 40), "Address      " + Profile.homeAddress, fill=(255, 255, 255))
-        d.text(
-            (40, 60), "Phone number      " + Profile.phoneNumber, fill=(255, 255, 255)
-        )
+        # """
+        # Write user info on the new created image
+        # """
+        # d = ImageDraw.Draw(imgCard)
+        # firstName = User.first_name
+        # lastName = User.last_name
+        # Address = Profile.homeAddress
+        # phone = Profile.phoneNumber
+        # d.text((90 + imgProfile_w, -20 + imgProfile_h), firstName, fill=(255, 255, 255))
+        # d.text((130 + imgProfile_w, -20 + imgProfile_h), lastName, fill=(255, 255, 255))
+        # d.text((90 + imgProfile_w, -5 + imgProfile_h), Address, fill=(255, 255, 255))
 
-        imgCard.paste(imgProfile)
-        imgCard.save(self.cardID.path)
+        # imgCard.paste(logoImg, offsetlogo)
+        # imgCard.paste(imgProfile, offset)
 
+        # imgCard.save(
+        #     "media/profile_images/card_ID/" + user.first_name + user.last_name + ".png"
+        # )
+        imgProfile_read.close()
+
+
+# """ Implementing generation of card ID per user """
+
+
+# class Card(models.Model):
+#     profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+
+#     cardID = models.ImageField(
+#         default="profile_images/card_ID/default_card.png",
+#         upload_to="profile_images/card_ID",
+#     )
+
+#     def save(self, *args, **kwargs):
+#         super().save(*args, **kwargs)
+#         imgProfile_w, imgProfile_h = imgProfile.size
+#         logoImg = Image.open(
+#             "static/img/logo/Romanian_Alpine_Club_Logo_transparent.png", "r"
+#         )
+#         logoImg_w, logoImg_h = logoImg.size
+#         imgCard = Image.new("RGB", (600, 350), color="#00AEEF")
+#         imgCard_w, imgCard_h = imgCard.size
+#         offsetlogo = (40, 10)
+#         offset = (40, 190)
+
+#         """
+#         Write user info on the new created image
+#         """
+#         d = ImageDraw.Draw(imgCard)
+#         firstName = User.first_name
+#         lastName = User.last_name
+#         Address = Profile.homeAddress
+#         phone = Profile.phoneNumber
+#         d.text((90 + imgProfile_w, -20 + imgProfile_h), firstName, fill=(255, 255, 255))
+#         d.text((130 + imgProfile_w, -20 + imgProfile_h), lastName, fill=(255, 255, 255))
+#         d.text((90 + imgProfile_w, -5 + imgProfile_h), Address, fill=(255, 255, 255))
+
+#         imgCard.paste(logoImg, offsetlogo)
+#         imgCard.paste(imgProfile, offset)
+
+#         imgCard.save(
+#             "media/profile_images/card_ID/" + user.first_name + user.last_name + ".png"
+#         )
