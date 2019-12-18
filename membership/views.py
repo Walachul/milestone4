56f49f1ed957dabc4e095be14c5936a99de3371d
Samuel <42890101+Walachul@users.noreply.github.com>
2019-12-18 14:26:51 +0100

    Created membershipselect view in order to display it in the template register.

--- membership/views.py
+++ membership/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from .models import Membership
+
+# Select membership out of 3 available
+class MembershipSelect(ListView):
+    model = Membership
+
+    template_name = "users/register.html"
+
+    context_object_name = "memberships"
