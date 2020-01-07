33e63bd29dc8c291f7803a3adbc29d7710a031d4
Samuel <42890101+Walachul@users.noreply.github.com>
2020-01-07 15:18:13 +0100

    Created views for viewing the cart content page and also to add items to the cart.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+
+
+def view_cart(request):
+    """
+    View the cart content page
+    """
+    return render(request, "cart/cart.html")
+
+
+def add_to_cart(request, id):
+    """
+    Add an item to the cart and increase or decrease quantity
+    """
+    quantity = int(request.POST.get("quantity"))
+
+    cart = request.session.get("cart", {})
+    cart[id] = cart.get(id, quantity)
+
+    request.session["cart"] = cart
+
+    return redirect(reverse("products-home"))
