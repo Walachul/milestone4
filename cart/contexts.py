3b506695a3f6b2e2eced00fa7a2c013ebadceaeb
Samuel <42890101+Walachul@users.noreply.github.com>
2020-01-07 21:14:49 +0100

    Working on error for quantity variable = NoneType, altough it is converted to integer.

--- cart/contexts.py
+++ cart/contexts.py
@@ -9,16 +9,15 @@ def cart_contents(request):
     Cart items are gone when the user logs out.
     """
     cart = request.session.get("cart", {})
-
     cart_items = []
     total = 0
     item_count = 0
-
+    quantity = 1
     for id, quantity in cart.items():
-        merch = get_object_or_404(Merchandise, pk=id)
-        total += quantity * merch.price
+        merchandise = get_object_or_404(Merchandise, pk=id)
+        total += int(quantity) * int(merchandise.price)
         item_count += quantity
-        cart_items.append({"id": id, "quantity": quantity, "merch": merch})
+        cart_items.append({"id": id, "quantity": quantity, "merchandise": merchandise})
 
     return {"cart_items": cart_items, "total": total, "item_count": item_count}
 
--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
-
+# Remember to add @login_required
 def view_cart(request):
     """
     View the cart content page
@@ -11,26 +11,20 @@ def view_cart(request):
 
 
 def add_to_cart(request, id):
-    """
-    Add an item to the cart and increase or decrease quantity
-    """
-    quantity = int(request.POST.get("quantity"))
+    """Add an item to the cart and increase or decrease quantity"""
 
+    quantity = int(request.POST.get("quantity"))
+    print(quantity)
     cart = request.session.get("cart", {})
-    if id in cart:
-        cart[id] = int(cart[id] + quantity)
-    else:
-        cart[id] = cart.get(id, quantity)
+    cart[id] = cart.get(id, quantity)
 
     request.session["cart"] = cart
 
-    return redirect(reverse("products-home"))
+    return redirect(reverse("view_cart"))
 
 
-def modify_cart(request, id):
-    """
-    Enables the user to modify the cart items quantity
-    """
+def modify_cart(request, int: id):
+    """Enables the user to modify the cart items quantity"""
     quantity = int(request.POST.get("quantity"))
     cart = request.session.get("cart", {})
 
