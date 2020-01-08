eee882a91aae721ad32763d0bcfa1f13df3ada0c
Samuel <42890101+Walachul@users.noreply.github.com>
2020-01-08 09:22:23 +0100

    Created models for checkout app in order to get information from user required for payment and to know which product was purchased.

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from products.models import Merchandise
 
-# Create your models here.
+"""Get valid information from user"""
+
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=100, blank=False)
+    phone_number = models.CharField(max_length=30, blank=False)
+    country = models.CharField(max_length=100, blank=False)
+    postcode = models.CharField(max_length=20, blank=False)
+    city = models.CharField(max_length=100, blank=False)
+    street_adress1 = models.CharField(max_length=100, blank=False)
+    street_adress2 = models.CharField(max_length=100, blank=False)
+    date = models.DateField()
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
+
+"""What product and how much quantity the user is buying"""
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, null=False)
+    merchandise = models.ForeignKey(Merchandise, null=False)
+    quantity = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "{0} {1} @ {2}".format(
+            self.quantity, self.merchandise.name, self.merchandise.price
+        )
