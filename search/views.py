ca9f9d2b2dbf46709ef064186b699382d43b1dea
Samuel <42890101+Walachul@users.noreply.github.com>
2020-01-08 12:39:43 +0100

    Search functionality moved to merchandise template and also usable only by logged in users.

--- search/views.py
+++ search/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from products.models import Merchandise
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required()
 def search_merch(request):
     merchandise = Merchandise.objects.filter(name__icontains=request.GET["q"])
     return render(request, "products/items.html", {"merchandise": merchandise})
