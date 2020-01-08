349c0bf75d88510b6362c49987837ae631c8b758
Samuel <42890101+Walachul@users.noreply.github.com>
2020-01-08 12:18:40 +0100

    Improved security for cart, checkout and products apps. Only logged in users are allowed to view them.Tests ok.

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-# Remember to add @login_required
+
+@login_required()
 def view_cart(request):
     """
     View the cart content page
@@ -10,6 +12,7 @@ def view_cart(request):
     return render(request, "cart/cart.html")
 
 
+@login_required()
 def add_to_cart(request, id):
     """Add an item to the cart and increase or decrease quantity"""
     quantity = int(request.POST.get("quantity"))
@@ -25,6 +28,7 @@ def add_to_cart(request, id):
     return redirect(reverse("products-home"))
 
 
+@login_required()
 def modify_cart(request, id):
     """Enables the user to modify the cart items quantity"""
     cart = request.session.get("cart", {})
--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Merchandise
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def all_items(request):
 
     merchandise = Merchandise.objects.all()
