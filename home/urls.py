de2c837495284756b301e02708304db91ab84afe
Samuel <42890101+Walachul@users.noreply.github.com>
2020-01-09 09:33:49 +0100

    Created partners view to render the partners template and to move partners from footer to the partners page.

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r"^$", views.home, name="home-home")]
+urlpatterns = [
+    url(r"^$", views.home, name="home-home"),
+    url(r"^partners/", views.partners, name="home-partners"),
+]
 
--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-
+# Home page
 def home(request):
-    return render(request, "index.html", {"title": "Home"})
+    return render(request, "home/index.html", {"title": "Home"})
+
+
+# Partners page
+def partners(request):
+    return render(request, "home/partners.html", {"title": "Partners"})
