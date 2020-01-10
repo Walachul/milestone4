6c5ec925b57d2142bb380ff0c588a38ed05a356e
Samuel <42890101+Walachul@users.noreply.github.com>
2020-01-10 13:08:14 +0100

    Modified the SECRET_KEY to get an environmental variable.

--- Romanian_Alpine_Club/settings.py
+++ Romanian_Alpine_Club/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "&&xb+b-d^g8lxmo)bi^h%nq)=ga5cwim)3905^nn71_dgijigq"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
