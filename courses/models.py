54f36eae65e967624a1d25b256a2fb23a22e8ed6
Samuel <42890101+Walachul@users.noreply.github.com>
2020-02-21 11:00:52 +0100

    Created Courses model for the courses app and registered it to admin.Made migrations.

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Courses(models.Model):
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    dateAdded = models.DateTimeField(default=timezone.now)
+    # Time period in which the course is organized
+    periodOfTime = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+
+    scopeOfCourse = models.CharField(max_length=200)
+    # Notions that will be discussed/learned during the course
+    notionDiscussed = models.TextField()
+    # Requirements for enrolling
+    requirementsForEnrolling = models.CharField(max_length=200, null=False)
+    requiredGear = models.CharField(max_length=200)
+    # Participants
+    participants = models.CharField(max_length=20)
+    participant = models.ForeignKey(User, default=None)
+    trainer = models.CharField(max_length=100)
