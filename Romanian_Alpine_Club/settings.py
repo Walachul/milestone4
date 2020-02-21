c7a0be76555f6ab116e6b2e1aee0b07863e116db
Samuel <42890101+Walachul@users.noreply.github.com>
2020-02-21 10:11:03 +0100

    Created app courses and registered it in settings.

--- Romanian_Alpine_Club/settings.py
+++ Romanian_Alpine_Club/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "cart.apps.CartConfig",
     "search.apps.SearchConfig",
     "checkout.apps.CheckoutConfig",
+    "courses.apps.CoursesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
