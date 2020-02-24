bb973cda330969996317671e9100e30938b5ce2c
Samuel <42890101+Walachul@users.noreply.github.com>
2020-02-24 21:10:52 +0100

    Removed field totalParticipants from Courses model and also removed it from displaying in the course_details template.

--- courses/models.py
+++ courses/models.py
@@ -13,7 +13,6 @@ class Courses(Product):
 
     # Participants
     participants = models.CharField(max_length=20)
-    totalParticipants = models.IntegerField(default=0)
     """In order to get the correct video_url and render it in the iframe,
         you have to get the Embed Video from YouTube and get link as:
         https://www.youtube.com/embed/exampleVideo
