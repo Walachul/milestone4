0e62e99dc923ffd7d58d33bddd7908e4c4f31920
Samuel <42890101+Walachul@users.noreply.github.com>
2020-02-26 21:36:47 +0100

    Setting up project for heroku deployment.

--- Romanian_Alpine_Club/settings.py
+++ Romanian_Alpine_Club/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["clubul-alpin-roman.herokuapp.com", "127.0.0.1"]
 
@@ -88,16 +88,16 @@ WSGI_APPLICATION = "Romanian_Alpine_Club.wsgi.application"
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-# if "DATABASE_URL" in os.environ:
-#     DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
-# else:
-#     print("Database URL not found. Using SQLite instead")
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+if "DATABASE_URL" in os.environ:
+    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
