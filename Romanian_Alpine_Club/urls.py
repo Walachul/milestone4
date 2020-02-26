f2cf7105314d91d076a008023539dc105a04ca0f
Samuel <42890101+Walachul@users.noreply.github.com>
2020-02-27 00:04:25 +0100

    Tracing error for profile not showing to Logo.png that Heroku does not find on given path. importing serve instead of static in main urls.py.

--- Romanian_Alpine_Club/urls.py
+++ Romanian_Alpine_Club/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import serve
 from django.views import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
