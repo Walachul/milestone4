e16375ccb3a21764876192ea01c400ec82a213c3
Samuel <42890101+Walachul@users.noreply.github.com>
2020-02-27 00:05:59 +0100

    Forgot to remove static from url media. Added serve function.

--- Romanian_Alpine_Club/urls.py
+++ Romanian_Alpine_Club/urls.py
@@ -82,6 +82,6 @@ urlpatterns = [
     url(r"^checkout/", include(urls_checkout)),
     url(r"^courses/", include(urls_courses)),
     url(r"^search/", include(urls_search)),
-    url(r"^media/(?P<path>.*)$", static.serve, {"document_root": MEDIA_ROOT}),
+    url(r"^media/(?P<path>.*)$", serve, {"document_root": MEDIA_ROOT}),
 ]
 
